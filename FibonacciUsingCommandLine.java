58059b4a0fab4b0710a18146da7a050c31c18250
unknown <nathis468@gmail.com>
2023-09-02 15:25:07 +0530

    first commit

---
+++ FibonacciUsingCommandLine.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Fibonacci{
+    static void fibonacciPrint(int end){
+        
+    }
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int end=sc.nextInt();
+        fibonacciPrint(end);
+    }
+}
+
