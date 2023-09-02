4a6062e31e757dc791e6d61896deec44ce5d1013
unknown <nathis468@gmail.com>
2023-09-02 15:56:45 +0530

    second commit

--- FibonacciUsingCommandLine.java
+++ FibonacciUsingCommandLine.java
@@ -1,14 +1,21 @@
 import java.util.Scanner;
 
-class Fibonacci{
-    static void fibonacciPrint(int end){
-        
+class FibonacciUsingCommandLine{
+    static void fibonacciPrint(int prev,int current,int end){
+        if(end>0){
+            int next=prev+current;
+            prev=current;
+            current=next;
+            end--;
+            fibonacciPrint(prev,current,end);
+            System.out.println(prev);
+        }
     }
 
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int end=sc.nextInt();
-        fibonacciPrint(end);
+        fibonacciPrint(0,1,end);
     }
 }
 
