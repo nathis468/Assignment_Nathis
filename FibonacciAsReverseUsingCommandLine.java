3c6f2639c4b455e2d7d7af338e1ddd31c2892b8c
unknown <nathis468@gmail.com>
2023-09-02 16:22:49 +0530

    third commit

--- FibonacciAsReverseUsingCommandLine.java
+++ FibonacciAsReverseUsingCommandLine.java
@@ -1,22 +1,22 @@
-// usinng recursion
+// using recursion
 
 import java.math.BigInteger;
 
-class FibonacciUsingCommandLine{
+class FibonacciAsReverseUsingCommandLine{
     static void fibonacciPrint(BigInteger prev,BigInteger current,int end){
         if(end>0){
-            System.out.println(prev);
             BigInteger next=prev.add(current);
             prev=current;
             current=next;
             end--;
             fibonacciPrint(prev,current,end);
+            System.out.println(current);
         }
     }
 
     public static void main(String args[]){
         int end=Integer.parseInt(args[0]);
-        fibonacciPrint(new BigInteger("0"),new BigInteger("1"),end);
+        fibonacciPrint(new BigInteger("-1"),new BigInteger("1"),end);
     }
 }
 
