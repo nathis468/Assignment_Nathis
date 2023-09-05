8a47637043c85a6980d2993531d7f8bfb7cda575
unknown <nathis468@gmail.com>
2023-09-05 09:47:38 +0530

    9th commit

---
+++ CopyConstructor2.java
@@ -0,0 +1,8 @@
+class CopyConstructor2 {
+    public static void main(String args[]){
+        CopyConstructor1 cc3=new CopyConstructor1();
+        CopyConstructor1 cc1=new CopyConstructor1("202ct119","Nathis");
+        CopyConstructor1 cc2=new CopyConstructor1(cc1);
+        CopyConstructor1 cc4=new CopyConstructor1(cc2);
+    }
+}
